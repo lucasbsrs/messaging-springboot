e3931fe5ade9d59c5921067b7d806a13866159ed
Lucas Batista da Silva <lucas.batista@paipe.co>
2023-08-07 23:46:42 -0300

    SpringBoot com Kafka

---
+++ src/test/java/br/com/lucassilva/messagingspringboot/MessagingSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucassilva.messagingspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
